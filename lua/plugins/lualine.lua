6bf2682140c7bf63c20a7f60d5860ac8b679a2df
henrybatt <hbatt1545@gmail.com>
2024-03-04 21:35:16 +1000

    lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+    	dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({})
+		end,
+	},
+}
