293f8e02150a7650a9510b74481cc9d2afe0c974
henrybatt <hbatt1545@gmail.com>
2024-03-04 21:35:34 +1000

    theme

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@ require("core.keymaps")
 require("core.options")
 require("core.auto_cmds")
 require("core.lazy")
+
+require('catppuccin').setup({transparent_background = false})
+vim.cmd.colorscheme("catppuccin")
