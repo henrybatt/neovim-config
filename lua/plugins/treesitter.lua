6afd8be3e6ae2ce2f28241494addeb1c24f65944
henrybatt <hbatt1545@gmail.com>
2024-03-04 21:35:29 +1000

    treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		enable = false,
+		event = { "BufReadPre", "BufNewFile" },
+		cmd = { "TSInstallInfo", "TSInstall" },
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-context",
+			"HiPhish/rainbow-delimiters.nvim",
+			 "nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+          		ensure_installed = {
+					"c", "cpp", "python", "lua", "vim",
+				},
+				ignore_install = {},
+          		sync_install = false,
+				auto_install = false,
+          		highlight = { enable = true },
+          		indent = { enable = true, disable = {"python"} },
+				autotag = { enable = true },
+				autopairs = { enable = true },
+				query_linter = {
+   	 				enable = true,
+    				use_virtual_text = true,
+    				lint_events = {"BufWrite", "CursorHold"},
+  				},
+
+        	})
+		end,
+	},
+}
