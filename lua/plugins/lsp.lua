7bce3f5a9149370a8dd63518dceaf0780f469b69
henrybatt <37820016+henrybatt@users.noreply.github.com>
2024-03-07 23:51:01 +1000

    Added lsp keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
 		cmd = "Mason",
 		dependencies = {
 			{ "williamboman/mason.nvim" },
-      		"williamboman/mason-lspconfig.nvim",
+			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			{ "j-hui/fidget.nvim", opts = {}, },
 			{"folke/neodev.nvim", opts = {}, },
@@ -21,9 +21,9 @@ return {
 						},
 						build = function ()
 							if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            					return
-          					end
-          					return 'make install_jsregexp'
+								return
+							end
+							return 'make install_jsregexp'
 						end,
 					}
 				},
@@ -32,8 +32,8 @@ return {
 		config = function()
 
 			-- LSP + Mason Setup -- 
-      		local capabilities = vim.lsp.protocol.make_client_capabilities()
-      		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 			local servers = {
 				lua_ls = {},
@@ -42,16 +42,16 @@ return {
 			require("mason").setup()
 
 			local ensure_installed = vim.tbl_keys(servers or {})
-      			vim.list_extend(ensure_installed, {
-      		})
+				vim.list_extend(ensure_installed, {
+			})
 
-      		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 			require("mason-lspconfig").setup({
 				handlers = {
 					function(server_name)
 						local server = servers[server_name] or {}
-			            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
 						require("lspconfig")[server_name].setup(server)
 					end,
 				}
@@ -67,13 +67,13 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 					['<C-l>'] = cmp.mapping(function()
-            				if luasnip.expand_or_locally_jumpable() then
-              					luasnip.expand_or_jump()
+							if luasnip.expand_or_locally_jumpable() then
+								luasnip.expand_or_jump()
 							end
 						end, { 'i', 's' }),
-          			['<C-h>'] = cmp.mapping(function()
-            				if luasnip.locally_jumpable(-1) then
-             					luasnip.jump(-1)
+					['<C-h>'] = cmp.mapping(function()
+							if luasnip.locally_jumpable(-1) then
+								luasnip.jump(-1)
 							end
 						end, { 'i', 's' }),
 				}),
@@ -92,6 +92,45 @@ return {
 					end,
 				},
 				completion = { completeopt = "menu,menuone,noinsert" },
+			})
+
+			-- Keymappings & automations
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+				callback = function(event)
+
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+
+					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+					map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+					map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+					map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+				  	map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+					map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+					map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+					map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+					map('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+					-- Highlight definitions when cursor holds on it
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if client and client.server_capabilities.documentHighlightProvider then
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = event.buf,
+							callback = vim.lsp.buf.document_highlight,
+						})
+
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = event.buf,
+							callback = vim.lsp.buf.clear_references,
+						})
+					end
+				end
 			})
 		end,
 	},
