3a378703bdcf4ebf49c4fb195ef3bab6b6133668
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-01-20 17:09:39 +1000

    Comment-nvim added
    Setup nvim comment to work
    Ignores empty lines when commenting blocks

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,12 @@
 return {
     "numToStr/Comment.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    opts = {},
+
+    opts = {
+        ignore = "^$",
+    },
+
+    config = function(_, opts)
+        require("Comment").setup(opts)
+    end,
 }
