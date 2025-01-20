470058bdf89fcc15ab27689f8eb90d67e3873f07
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-01-20 17:10:53 +1000

    Extend captppuccin integrations
    Modify integrations to include additional such as notify, etc

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,6 +5,24 @@ return {
         priority = 1000,
         config = function()
             vim.cmd.colorscheme "catppuccin"
+            require("catppuccin").setup({
+                transparent_background = true,
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    treesitter = true,
+                    notify = true,
+                    telescope = { enabled = true },
+                    barbar = true,
+                    alpha = true,
+                    fidget = true,
+                    hop = true,
+                    mason = true,
+                    noice = true,
+                    which_key = true,
+                },
+            })
         end,
     },
     {
@@ -12,45 +30,41 @@ return {
         lazy = false,
     },
     {
-    'goolord/alpha-nvim',
-    event = 'VimEnter',
+    "goolord/alpha-nvim",
+    event = "VimEnter",
     config = function()
-        local opts = { noremap = true, silent = true }
-
-        vim.keymap.set('n', '<leader>a', ':Alpha<CR>', opts)
-
-        local status_ok, alpha = pcall(require, 'alpha')
+        local status_ok, alpha = pcall(require, "alpha")
         if not status_ok then
             return
         end
 
-        local dashboard = require('alpha.themes.dashboard')
+        local dashboard = require("alpha.themes.dashboard")
         dashboard.section.buttons.val = {
-            dashboard.button('f', '  Find file', ':Telescope find_files <CR>'),
-            dashboard.button('e', '  New file', ':ene <CR>'),
-            dashboard.button('r', '  Recently used files', ':Telescope oldfiles <CR>'),
-            dashboard.button('gr', '  Find text', ':Telescope live_grep <CR>'),
-            dashboard.button('n', '  Open file tree', ':NvimTreeOpen <CR>'),
-            dashboard.button('q', '  Quit Neovim', ':qa<CR>'),
+            dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+            dashboard.button("e", "  New file", ":ene <CR>"),
+            dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+            dashboard.button("gr", "  Find text", ":Telescope live_grep <CR>"),
+            dashboard.button("n", "  Open file tree", ":NvimTreeOpen <CR>"),
+            dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
         }
 
         local v = vim.version()
-        local version = ' v' .. v.major .. '.' .. v.minor .. '.' .. v.patch
-        local datetime = os.date(' %d-%m-%Y 󱑏 %H:%M:%S')
+        local version = " v" .. v.major .. "." .. v.minor .. "." .. v.patch
+        local datetime = os.date(" %d-%m-%Y 󱑏 %H:%M:%S")
 
-        vim.api.nvim_create_autocmd('User', {
-            pattern = 'LazyVimStarted',
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "LazyVimStarted",
             callback = function()
-                local stats = require('lazy').stats()
+                local stats = require("lazy").stats()
                 local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
                 dashboard.section.footer.val = {
-                    '',
-                    '',
+                    "",
+                    "",
                     version,
-                    '',
+                    "",
                     datetime,
-                    '',
-                    '⚡ Neovim loaded ' .. stats.count .. ' plugins in ' .. ms .. 'ms',
+                    "",
+                    "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms",
                 }
                 pcall(vim.cmd.AlphaRedraw)
             end,
@@ -58,9 +72,9 @@ return {
 
         dashboard.section.footer.val = {}
 
-        dashboard.section.footer.opts.hl = 'Type'
-        dashboard.section.header.opts.hl = 'Include'
-        dashboard.section.buttons.opts.hl = 'Keyword'
+        dashboard.section.footer.opts.hl = "Type"
+        dashboard.section.header.opts.hl = "Include"
+        dashboard.section.buttons.opts.hl = "Keyword"
 
         dashboard.opts.opts.noautocmd = true
         alpha.setup(dashboard.opts)
