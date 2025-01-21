31160bf23fddc92bc188cb2f26d3e5a42dd7842f
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-01-22 09:07:38 +1000

    Added word counter to lualine
    Adds a word count to lualine bar for specific file types.
    Inspired by mattyoung101: https://github.com/mattyoung101/dotfiles

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,27 @@
+local countableFileTypes = {}
+local function addTypes(types)
+    for _, type in ipairs(types) do
+        countableFileTypes[type] = true
+    end
+end
+addTypes({ "md", "txt", "markdown", "tex", "latex", "typst", "typ" })
+
 return {
     {
         "nvim-lualine/lualine.nvim",
         event = { "BufReadPre", "BufNewFile" },
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup({})
-        end,
+        opts = {
+           sections = {
+               lualine_x = {
+                    function()
+                        if countableFileTypes[vim.bo.filetype] then
+                            return (vim.fn.wordcount().visual_words or vim.fn.wordcount().words) .. " words"
+                        end
+                        return ""
+                    end,
+                    "oil", "encoding", "fileformat", "filetype"}
+           }
+        },
     },
 }
