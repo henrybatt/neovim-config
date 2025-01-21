8fd2737f16d1013ac5ce1a6fefd1822e3e434c1e
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-01-22 09:10:07 +1000

    Change telescope keybinding style
    Does not change behaviour
    I just cannot read correctly and did it wrong before
    Now actually shows the descriptions of keybindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -106,27 +106,28 @@ return {
         -- Keymaps
         keys = function()
             local builtin = require("telescope.builtin")
+            return {
+                { "<leader><space>", builtin.buffers,   desc = "[ ] Find existing buffers"  },
+                { "<leader>/",  builtin.current_buffer_fuzzy_find,  desc = "[/] Fuzzily search in current buffer" },
+                { "<leader>?",  builtin.oldfiles,       desc = "[?] Find recently opened files" },
+                { "<leader>.",  builtin.resume,         desc = "[.] Resume Previous Telescope" },
 
-            vim.keymap.set("n", "<leader><space>", builtin.buffers,   { desc = "[ ] Find existing buffers" })
-            vim.keymap.set("n", "<leader>/",  builtin.current_buffer_fuzzy_find,  { desc = "[/] Fuzzily search in current buffer" })
-            vim.keymap.set("n", "<leader>?",  builtin.oldfiles,       { desc = "[?] Find recently opened files" })
-            vim.keymap.set("n", "<leader>.",  builtin.resume,         { desc = "[.] Resume Previous Telescope" })
+                { "<leader>sf", builtin.find_files,     desc = "[S]earch [F]iles" },
 
-            vim.keymap.set("n", "<leader>sf", builtin.find_files,     { desc = "[S]earch [F]iles" })
+                { "<leader>sw", builtin.grep_string,    desc = "[S]earch current [W]ord" },
+                { "<leader>sg", builtin.live_grep,      desc = "[S]earch by [G]rep" },
 
-            vim.keymap.set("n", "<leader>sw", builtin.grep_string,    { desc = "[S]earch current [W]ord" })
-            vim.keymap.set("n", "<leader>sg", builtin.live_grep,      { desc = "[S]earch by [G]rep" })
+			    { "<leader>sd", builtin.diagnostics,    desc = "[S]earch [D]iagnostics" },
+			    { "<leader>ss", builtin.builtin,        desc = "[S]earch [S]elect Telescope" },
+			    { "<leader>sh", builtin.help_tags,      desc = "[S]earch [H]elp" },
+                { "<leader>sk", builtin.keymaps,        desc = "[S]earch [K]eymaps" },
 
-            vim.keymap.set("n", "<leader>sd", builtin.diagnostics,    { desc = "[S]earch [D]iagnostics" })
-            vim.keymap.set("n", "<leader>ss", builtin.builtin,        { desc = "[S]earch [S]elect Telescope" })
-            vim.keymap.set("n", "<leader>sh", builtin.help_tags,      { desc = "[S]earch [H]elp" })
-            vim.keymap.set("n", "<leader>sk", builtin.keymaps,        { desc = "[S]earch [K]eymaps" })
+                { "<leader>sn", function() builtin.find_files { cwd = vim.fn.stdpath "config" } end,    desc = "[S]earch [N]eovim Files" },
 
-            vim.keymap.set("n", "<leader>sn", function() builtin.find_files { cwd = vim.fn.stdpath "config" } end, { desc = "[S]earch [N]eovim Files" })
-
-            vim.keymap.set("n", "<leader>sy", "<cmd>Telescope neoclip<CR>", { desc = "[Search] Previous [Y]anked Content " })
-            vim.keymap.set("n", "<leader>su", "<cmd>Telescope undo<CR>",      { desc = "[S]earch [U]ndo Tree" })
-            vim.keymap.set("n", "<leader>sr", "<Cmd>Telescope frecency<CR>",  { desc = "[S]earch F[R]ecency" })
+                { "<leader>sy", "<cmd>Telescope neoclip<CR>",   desc = "[Search] Previous [Y]anked Content " },
+                { "<leader>su", "<cmd>Telescope undo<CR>",      desc = "[S]earch [U]ndo Tree" },
+                { "<leader>sr", "<Cmd>Telescope frecency<CR>",  desc = "[S]earch F[R]ecency" },
+            }
         end,
     }
 }
