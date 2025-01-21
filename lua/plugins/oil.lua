b8bd68f450b1c6d46890fd1c5d91a482a8d53495
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-01-22 09:09:10 +1000

    Added oil
    Opens in a floating window
    Open parent with "-" whilst in normal mode
    "q" to exit
    "gd" shows file details

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,39 @@
+return {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
+
+    ---@module "oil"
+    ---@type oil.SetupOpts
+    opts = {
+        delete_to_trash = false,
+        float = {
+            padding = 4,
+        },
+        keymaps = {
+            ["gd"] = {
+                desc = "Toggle file detail view",
+                callback = function()
+                    detail = not detail
+                    if detail then
+                        require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+                    else
+                        require("oil").set_columns({ "icon" })
+                    end
+                end,
+            },
+            ["q"] = { "actions.close", mode = "n", desc = "Close OIL" }
+        },
+        view_options = {
+            show_hidden = true,
+            is_always_hidden = function(name, _)
+                return vim.startswith(name, '.DS_Store')
+            end,
+        },
+    },
+
+    keys = function()
+        return {
+            { "-", require("oil").open_float, desc = { "Open parent directory - OIL" } },
+        }
+    end,
+}
