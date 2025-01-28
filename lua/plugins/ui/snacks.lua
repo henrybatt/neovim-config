37af0cdc9e80281ca4ccca1e2820135eea275f45
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-01-28 22:09:21 +1000

    Added snacks.nvim to add new features and combine old
    Features used:
    - indent
    - input
    - notifier (replaced nvim-notify)
    - scope
    - scroll
    - statuscolumn
    - words

---
+++ lua/plugins/ui/snacks.lua
@@ -0,0 +1,15 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+
+    opts = {
+        indent = { enabled = true },
+        input = { enabled = true },
+        notifier = { enabled = true },
+        scope = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = true },
+        words = { enabled = true },
+    },
+}
