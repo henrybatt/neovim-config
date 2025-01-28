a38a965954f7caa53095b93de138cdadd7072684
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-01-28 22:39:42 +1000

    Modify lazygit config for performance
    Change keybinding behaviour.
    
    Only load on command usage to speed up init times

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,10 +1,15 @@
 return {
     "kdheepak/lazygit.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
     },
 
-    keys = function()
-        vim.keymap.set("n", "<leader>lg", require("lazygit").lazygit, { desc = "[L]azy[G]it" })
-    end,
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
 }
