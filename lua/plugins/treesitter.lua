2a84ee292756fa881428579280ab6b82d4533520
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-01-28 21:56:46 +1000

    Re-enable treesitter - neovim 0.10.x

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,40 +1,38 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-        version = "0.8.5.2",
-		enable = true,
-		-- event = { "BufReadPre", "BufNewFile" },
-		-- cmd = { "TSInstallInfo", "TSInstall" },
-		build = ":TSUpdate",
-		dependencies = {
-		    -- "nvim-treesitter/nvim-treesitter-context",
-		    "HiPhish/rainbow-delimiters.nvim",
-		    --"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		config = function()
-			local configs = require("nvim-treesitter.configs")
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        cmd = { "TSInstallInfo", "TSInstall" },
+        build = ":TSUpdate",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-context",
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "HiPhish/rainbow-delimiters.nvim",
+        },
+        config = function()
+            local configs = require("nvim-treesitter.configs")
 
-			configs.setup({
-			    ensure_installed = {
-				    -- "c", "cpp", "lua", "python", "rust", "vimdoc", "vim", "markdown", "markdown_inline",
-				    -- "jsonc", "cmake", "bibtex", "fish", "make", "javascript", "php", "verilog", "yaml", "toml", "html",
-				    -- "javascript", "java", "kotlin", "dockerfile", "cuda", "query", "css", "ini", "rust", "glsl"
-			    },
-				ignore_install = {},
-				sync_install = false,
-				auto_install = false,
-				highlight = { enable = true },
-				indent = { enable = true, disable = {"python"} },
-				autotag = { enable = true },
-				autopairs = { enable = true },
-				query_linter = {
-					enable = true,
-					use_virtual_text = true,
-					lint_events = {"BufWrite", "CursorHold"},
-				},
+            configs.setup({
+                ensure_installed = {
+                    -- "c", "cpp", "lua", "python", "rust", "vimdoc", "vim", "markdown", "markdown_inline",
+                    -- "jsonc", "cmake", "bibtex", "fish", "make", "javascript", "php", "verilog", "yaml", "toml", "html",
+                    -- "javascript", "java", "kotlin", "dockerfile", "cuda", "query", "css", "ini", "rust", "glsl"
+                },
+                ignore_install = {},
+                sync_install = false,
+                auto_install = false,
+                highlight = { enable = true },
+                indent = { enable = true, disable = { "python" } },
+                autotag = { enable = true },
+                autopairs = { enable = true },
+                query_linter = {
+                    enable = true,
+                    use_virtual_text = true,
+                    lint_events = { "BufWrite", "CursorHold" },
+                },
                 update_strategy = "lockfile",
                 modules = {},
-			})
-		end,
-	},
+            })
+        end,
+    },
 }
