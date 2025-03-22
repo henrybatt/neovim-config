37da53abdf4a3c1edf00b736a822918fe684d937
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-03-22 20:40:34 +1000

    Update bufferline keys

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -4,10 +4,11 @@ return {
     event = "VeryLazy",
 
     keys = {
-        { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+        { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Buffer Pin" },
         { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
         { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
         { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+        { "<leader>bc", function() Snacks.bufdelete.delete() end, desc = "Delete Current Open Buffer"},
         { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
         { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
         { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
