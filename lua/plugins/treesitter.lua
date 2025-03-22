d3a3da4d1e3ec7614018c0f5bbfeb776841c296d
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-03-22 20:45:20 +1000

    Update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -20,8 +20,11 @@ return {
                 },
                 ignore_install = {},
                 sync_install = false,
-                auto_install = false,
-                highlight = { enable = true },
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false
+                },
                 indent = { enable = true, disable = { "python" } },
                 autotag = { enable = true },
                 autopairs = { enable = true },
