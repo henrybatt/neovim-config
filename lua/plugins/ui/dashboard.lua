617e0795053d78dcaf55ba218bb0a040b0dcaf2e
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-03-22 20:40:38 +1000

    Add contents to dashboard

--- lua/plugins/ui/dashboard.lua
+++ lua/plugins/ui/dashboard.lua
@@ -1,5 +1,8 @@
 return {
     "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+
     opts = {
         dashboard = {
             sections = {
@@ -21,6 +24,18 @@ return {
                 { section = "keys", gap = 1, padding = 1 },
                 { section = "startup" },
             },
+            keys = {
+                { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+                { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+                { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+            },
+
         }
     }
 }
