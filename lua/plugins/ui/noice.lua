fdb23128b56a6b23ed963366736aa45f80edfd38
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-08-17 02:55:05 +1000

    feat: add noice

---
+++ lua/plugins/ui/noice.lua
@@ -0,0 +1,34 @@
+return {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        { "MunifTanjim/nui.nvim" },
+    },
+
+    opts = {
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
+            },
+        },
+        views = {
+            cmdline_popup = {
+                position = {
+                    row = 7,
+                    col = "55%",
+                },
+            },
+            cmdline_popupmenu = {
+                position = {
+                    row = 7,
+                    col = "55%",
+                },
+            },
+        },
+    },
+}
+
+
+-- return {}
