a7be7be0e5ba630e80016e0a3fe7eaaf543e523c
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-08-17 02:54:32 +1000

    feat: utils - add requires joiner functions
    Function that allows for the combining of plugin tables.
    Function to require all .lua files in a directory

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local nvim_config_lua_path = vim.fn.stdpath("config") .. "/" .. "lua"
+
 function M.safe_require_alert(module_name)
     local mod = M.safe_require(module_name)
     if mod == nil then
@@ -41,6 +43,32 @@ function M.map(mode, keys, command, opts)
         options = vim.tbl_extend("force", options, opts)
     end
     vim.keymap.set(mode, keys, command, options)
+end
+
+--- Combines all plugin tables in a given package (directory)
+--- Excludes those outlined in the init.lua or files starting with "_" or ".".
+---@param package relative path in relation to nvim config/lua
+---@return table with all the plugins + configs
+function M.plugin_require(package)
+    local plugins = {}
+    for _, path in pairs(vim.split(vim.fn.glob(nvim_config_lua_path .. "/" .. package .. "/" .. "*.lua"), "\n")) do
+        local relative_module_path = path:gsub(nvim_config_lua_path, ""):gsub("%.lua", "")
+        local module_name = relative_module_path:gsub("(.*/)(.*)", "%2")
+        if (module_name ~= "init" and module_name:sub(1, 1) ~= "_" and module_name:sub(1, 1) ~= ".") then
+            table.insert(plugins, require(relative_module_path))
+        end
+    end
+    return plugins
+end
+
+function M.directory_require(directory)
+    for _, path in pairs(vim.split(vim.fn.glob(nvim_config_lua_path .. "/" .. directory .. "/" .. "*.lua"), "\n")) do
+        local relative_module_path = path:gsub(nvim_config_lua_path, ""):gsub("%.lua", "")
+        local module_name = relative_module_path:gsub("(.*/)(.*)", "%2")
+        if (module_name ~= "init" and module_name:sub(1, 1) ~= "_" and module_name:sub(1, 1) ~= ".") then
+            require(relative_module_path)
+        end
+    end
 end
 
 return M
