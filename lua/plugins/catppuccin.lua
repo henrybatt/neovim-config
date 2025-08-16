30ab963d1effb9c931bb3cc82dfb0c6192217393
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-08-16 14:34:22 +1000

    feat: add catppuccin theme

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+
+        config = function(_, opts)
+            require("catppuccin").setup(opts)
+            vim.cmd.colorscheme "catppuccin"
+        end,
+
+        opts = {
+            transparent_background = true,
+        },
+    },
+}
