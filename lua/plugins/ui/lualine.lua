91e552bfaf41f084e26cf5622c18a6dd27aeb7ad
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-08-17 14:09:42 +1000

    feat: add lualine

---
+++ lua/plugins/ui/lualine.lua
@@ -0,0 +1,33 @@
+local countableFileTypes = {}
+local function addTypes(types)
+    for _, type in ipairs(types) do
+        countableFileTypes[type] = true
+    end
+end
+addTypes({ "md", "txt", "markdown", "tex", "latex", "typst", "typ" })
+
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+
+    init = function()
+        vim.g.lualine_laststatus = vim.o.laststatus
+        if vim.fn.argc(-1) > 0 then
+            vim.o.statusline = " "
+        end
+    end,
+
+    opts = {
+        sections = {
+            lualine_x = {
+                function()
+                    if countableFileTypes[vim.bo.filetype] then
+                        return (vim.fn.wordcount().visual_words or vim.fn.wordcount().words) .. " words"
+                    end
+                    return ""
+                end,
+                "oil", "encoding", "fileformat", "filetype" }
+        }
+    },
+}
+
