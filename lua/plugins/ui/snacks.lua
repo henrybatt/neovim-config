615b4c84963af7678bb725f095bd92a9927f94c0
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-08-17 13:37:55 +1000

    feat: ui - added snacks ui items

---
+++ lua/plugins/ui/snacks.lua
@@ -0,0 +1,16 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+
+    opts = {
+        indent = { enabled = true },
+        input = { enabled = true },
+        notifier = { enabled = true },
+        notify = { enabled = true },
+        scope = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = true },
+        words = { enabled = true },
+    },
+}
