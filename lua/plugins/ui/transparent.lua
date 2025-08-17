38b332610e09f9084b8fde2a1650e64c7004e5e1
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-08-17 10:41:08 +1000

    feat: add transparent.nvim
    used to make floatings transparent

---
+++ lua/plugins/ui/transparent.lua
@@ -0,0 +1,12 @@
+return {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+
+    opts = {
+        extra_groups = {
+            "NormalFloat",
+            "FloatBorder",
+            "FloatTitle",
+        },
+    },
+}
