1c7bc198d246bdd91bcc3b1485474cbe44770794
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-08-17 14:00:41 +1000

    feat: remove telescope

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,18 +9,8 @@ local function on_attach(client, buf)
     end
 
     -- Keymappings
-    map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-    map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-    map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
-    map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
     map("<leader>gd", vim.diagnostic.open_float, "Open [D]iagnostic")
-
-    map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-    map("<leader>lds", require("telescope.builtin").lsp_document_symbols, "[L]SP [D]ocument [S]ymbols")
-    map("<leader>lws", require("telescope.builtin").lsp_dynamic_workspace_symbols,
-        "[L]SP [W]orkspace [S]ymbols")
     map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
     map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
     map("<leader>fmt", vim.lsp.buf.format, "LSP [F]or[m]a[t]")
