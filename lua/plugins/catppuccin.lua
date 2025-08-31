df5a0cb3e9fc3e5a1c9b7a96e6f1f987b590dceb
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-08-31 15:50:32 +1000

    feat: catppuccin - update integrations

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -10,5 +10,15 @@ return {
 
     opts = {
         transparent_background = true,
+        integrations = {
+            blink_cmp = true,
+            gitsigns = true,
+            mason = true,
+            mini = { enabled = true },
+            noice = true,
+            snacks = { enabled = true },
+            lsp_trouble = false,
+            which_key = true,
+        },
     },
 }
