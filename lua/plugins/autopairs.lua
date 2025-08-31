2a4eba36137699bfe879e3b28edabff34758c6f7
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-08-31 15:37:44 +1000

    feat: autopairs - enable ts checking

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,5 +1,11 @@
 return {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    config = true,
+
+    opts = {
+        check_ts = true,
+        ts_config = {
+            lua = { "string" },
+        },
+    },
 }
