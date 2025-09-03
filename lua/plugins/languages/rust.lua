6a8c44f24ce3b9f0223b316060c6b35c5ec2f3ce
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-09-03 16:21:25 +1000

    feat: lang - rust update config to ensure rustfmt

--- lua/plugins/languages/rust.lua
+++ lua/plugins/languages/rust.lua
@@ -1,3 +1,18 @@
+local function ensure_rustfmt()
+    if vim.fn.executable("rustfmt") == 0 and vim.fm.executable("rustup") ~= 0 then
+        vim.notify("Installing rustfmt via rustup...", vim.log.levels.INFO)
+        vim.fn.jobstart({ "rustup", "component", "add", "rustfmt" }, {
+            on_exit = function(_, code)
+                if code == 0 then
+                    vim.notify("rustfmt installed successfully", vim.log.levels.INFO)
+                else
+                    vim.notify("Failed to install rustfmt", vim.log.levels.ERROR)
+                end
+            end,
+        })
+    end
+end
+
 return require("core.utils").apply_ft("rust", {
     {
         "lsp-config",
@@ -6,7 +21,7 @@ return require("core.utils").apply_ft("rust", {
             {
                 "WhoIsSethDaniel/mason-tool-installer.nvim",
                 opts_extend = { "ensure_installed" },
-                opts = { ensure_installed = { "rust-analyzer" } },
+                opts = { ensure_installed = { "rust-analyzer", "codelldb" } },
             },
         },
 
@@ -22,7 +37,12 @@ return require("core.utils").apply_ft("rust", {
     {
         "nvim-treesitter/nvim-treesitter",
         opts = function()
+            ensure_rustfmt()
             require("nvim-treesitter").install({ "rust", "toml" })
         end,
     },
+    {
+        "mrcjkb/rustaceanvim",
+        lazy = false,
+    }
 })
