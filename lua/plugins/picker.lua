b1988b049ec6321aa3c9af0764b684578800a550
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-10-28 14:51:30 +1000

    fix: picker - change notifcations view to <leader>sn

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -72,7 +72,7 @@ return {
         },
         { "<leader>/", function() Snacks.picker.grep() end,            desc = "Grep" },
         { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
-        { "<leader>n", function() Snacks.picker.notifications() end,   desc = "Notification History" },
+        { "<leader>sn", function() Snacks.picker.notifications() end,   desc = "Notification History" },
 
         -- Find
         {
